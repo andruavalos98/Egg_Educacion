0721bea306f0ea2bd14191335e70b3ed0d4a803a
andruavalos98 <andruavalos15@gmail.com>
2022-03-22 22:01:45 -0300

    Mas seguridad implementada

---
+++ LibreriaSpring/libreria/src/main/java/com/libreria/libreria/controladores/UsuarioController.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.libreria.libreria.controladores;
+
+import com.libreria.libreria.entidades.Usuario;
+import com.libreria.libreria.servicios.UsuarioServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ *
+ * @author andru
+ */
+@Controller
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioServicio usuarioServicio;
+
+    @GetMapping("")
+    public String registro(Model modelo) {
+        modelo.addAttribute("username", "");
+        modelo.addAttribute("password", "");
+        modelo.addAttribute("password2", "");
+        return "usuario-formulario";
+    }
+
+    @PostMapping("/registro")
+    public String registroUsuario(@RequestParam("username") String username,
+            @RequestParam("password") String password,
+            @RequestParam("password2") String password2,
+            Model modelo) {
+
+        try {
+            Usuario usuario = usuarioServicio.registrarUsuario(username, password, password2);
+            modelo.addAttribute("success", "Usuario registrado con exito");
+
+            return "usuario-formulario";
+        } catch (Exception ex) {
+            modelo.addAttribute("username", username);
+            modelo.addAttribute("password", password);
+            modelo.addAttribute("password2", password2);
+            modelo.addAttribute("error", ex.getMessage());
+            return "usuario-formulario";
+        }
+    }
+
+}
--- LibreriaSpring/libreria/src/main/java/com/libreria/libreria/servicios/UsuarioServicio.java
+++ LibreriaSpring/libreria/src/main/java/com/libreria/libreria/servicios/UsuarioServicio.java
@@ -7,10 +7,15 @@ package com.libreria.libreria.servicios;
 
 import com.libreria.libreria.entidades.Usuario;
 import com.libreria.libreria.repositorios.UsuarioRepositorio;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -18,24 +23,44 @@ import org.springframework.stereotype.Service;
  * @author andru
  */
 @Service
-public class UsuarioServicio implements UserDetailsService{
-    
+public class UsuarioServicio implements UserDetailsService {
+
     @Autowired
     private UsuarioRepositorio usuarioRepositorio;
-    
-    public Usuario registrarUsuario(String username, String password, String password2){
-        Usuario usuario = new Usuario();
+
+    public Usuario registrarUsuario(String username, String password, String password2) throws Exception {
+        Usuario usuario = usuarioRepositorio.findByUsername(username);
+
+        if (username.isEmpty()) {
+            throw new Exception("El username no puede estar vacio");
+
+        }
+        if (usuario != null) {
+            throw new Exception("El usuario ya existe, pruebe otro nombre");
+        }
+        if (!password.equals(password2)) {
+            throw new Exception("Las contraseñas ingresadas deben ser iguales");
+
+        }
+        usuario = new Usuario();
         usuario.setUsername(username);
-        usuario.setPassword(password);
-        
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        usuario.setPassword(encoder.encode(password));
+
         return usuarioRepositorio.save(usuario);
-        
+
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        try {
+            Usuario usuario = usuarioRepositorio.findByUsername(username);
+            List<GrantedAuthority> autoritties = new ArrayList<>();
+
+            return new User(username, usuario.getPassword(), autoritties);
+        } catch (Exception e) {
+            throw new UsernameNotFoundException("El usuario no existe");
+        }
     }
-    
-   
+
 }
